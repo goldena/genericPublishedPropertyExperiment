803da3d15510ace1a3a18bd598bccd3b8582517e
Denis Goloborodko <goldena_by@outlook.com>
2022-10-01 21:59:17 +0300

    Initial Commit

---
+++ genericPublishedPropertyExperiment/genericPublishedPropertyExperimentApp.swift
@@ -0,0 +1,17 @@
+//
+//  genericPublishedPropertyExperimentApp.swift
+//  genericPublishedPropertyExperiment
+//
+//  Created by Denis Goloborodko on 1.10.22.
+//
+
+import SwiftUI
+
+@main
+struct genericPublishedPropertyExperimentApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
